0a62ed2a96b5119f5f71281fd2001e5e74990c3a
Agnieszka K. Kocinski <agnieszka.kocinski@gmail.com>
2015-06-21 15:10:37 +0100

    Model game type.

---
+++ src/RiftRunBundle/Model/GameType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace RiftRunBundle\Model;
+
+/**
+ * GameType
+ */
+class GameType
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
