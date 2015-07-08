a2cf7cbbba4646ec737c0daaf8b485ac65ab9256
Agnieszka K. Kocinski <agnieszka.kocinski@gmail.com>
2015-07-08 22:18:12 +0100

    Tags and stuff.

--- src/DevHelperBundle/Command/Handlers/DatabaseClearCommandHandler.php
+++ src/DevHelperBundle/Command/Handlers/DatabaseClearCommandHandler.php
@@ -18,26 +18,28 @@ final class DatabaseClearCommandHandler
     {
         $connection = $this->entityManager->getConnection();
 
-        $q = $this->entityManager->createQuery('delete from RiftRunBundle\Model\Post m');
-        $numDeleted = $q->execute();
-        echo $numDeleted;
+        // $q = $this->entityManager->createQuery('delete from RiftRunBundle\Model\Post m');
+        // $numDeleted = $q->execute();
+        // echo $numDeleted;
 
-        // $connection->exec('DELETE FROM characters');
-        // $connection->exec('DELETE FROM posts');
-        // $connection->exec('DELETE FROM grift');
-        // $connection->exec('DELETE FROM searchquery');
-        // $connection->exec('DELETE FROM gametype');
-        // $connection->exec('DELETE FROM characterclass');
 
-        // $connection->exec('DROP TABLE characters');
-        // $connection->exec('DROP TABLE posts');
-        // $connection->exec('DROP TABLE grift');
-        // $connection->exec('DROP TABLE searchquery');
-        // $connection->exec('DROP TABLE gametype');
-        // $connection->exec('DROP TABLE characterclass');
+        $connection->exec('DELETE FROM characters');
+        $connection->exec('DELETE FROM posts');
+        $connection->exec('DELETE FROM grift');
+        $connection->exec('DELETE FROM searchquery');
+        $connection->exec('DELETE FROM gametype');
+        $connection->exec('DELETE FROM characterclass');
+
+        $connection->exec('DROP TABLE characters');
+        $connection->exec('DROP TABLE posts');
+        $connection->exec('DROP TABLE grift');
+        $connection->exec('DROP TABLE searchquery');
+        $connection->exec('DROP TABLE gametype');
+        $connection->exec('DROP TABLE characterclass');
 
         //$connection->beginTransaction();
-        $connection->exec('VACUUM characters');
+        // $connection->exec('VACUUM characters');
+        // $connection->project('VACUUM characters');
         //$connection->commit();
         //$connection->commit();
     }
