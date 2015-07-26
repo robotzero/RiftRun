23e2869f7c087f1911b223f8009ecbbbb846e5d0
Agnieszka K. Kocinski <agnieszka.kocinski@gmail.com>
2015-07-26 13:14:25 +0100

    Phpspec test for AllPostsSpecification.

---
+++ spec/RiftRunBundle/ORM/Specification/AllPostsSpecificationSpec.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace spec\RiftRunBundle\ORM\Specification;
+
+use Doctrine\ORM\QueryBuilder;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class AllPostsSpecificationSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('RiftRunBundle\ORM\Specification\AllPostsSpecification');
+    }
+
+    // function it_implements_an_interface()
+    // {
+    //     $this->shouldImplement('RiftRunBundle\ORM\Specification');
+    // }
+
+    function it_should_build_fetch_all_posts_query(QueryBuilder $queryBuilder)
+    {
+        $queryBuilder->select('posts')->shouldBeCalled()->willReturn($queryBuilder);
+        $queryBuilder->innerJoin('posts.query', 'q')->shouldBeCalled()->willReturn($queryBuilder);
+        $queryBuilder->innerJoin('posts.player', 'p')->shouldBeCalled()->willReturn($queryBuilder);
+        $queryBuilder->innerJoin('q.game', 'g')->shouldBeCalled()->willReturn($queryBuilder);
+        $queryBuilder->innerJoin('q.characterClass', 'cl')->shouldBeCalled()->willReturn($queryBuilder);
+        $queryBuilder->add('where', 'posts.createdAt > :createdAt')->shouldBeCalled()->willReturn($queryBuilder);
+        $queryBuilder->setParameter('createdAt', Argument::type('\DateTime'), 'datetime')->shouldBeCalled()->willReturn($queryBuilder);
+
+        $this->__invoke($queryBuilder);
+    }
+}
