1300568471bb77155ca0e5ec86d3f8532cac13da
Agnieszka K. Kocinski <agnieszka.kocinski@gmail.com>
2015-11-13 19:02:48 +0000

    Trying to hydrating the object.

--- fa/components/post/post.ts
+++ fa/components/post/post.ts
@@ -1,9 +1,10 @@
 import {Component, View, NgFor, NgIf, CORE_DIRECTIVES} from 'angular2/angular2';
 import {APIGetService} from '../../services/apigetservice';
+import {ObjectHydrator} from "../../services/objecthydrator";
 
 @Component({
     selector: 'post',
-    providers:[APIGetService]
+    providers:[APIGetService, ObjectHydrator]
 })
 @View({
     templateUrl: './components/post/post.html',
@@ -13,8 +14,17 @@ import {APIGetService} from '../../services/apigetservice';
 export class Post {
     items: Array<string>;
 
-    constructor(getService:APIGetService) {
+    constructor(getService:APIGetService, public hydrator: ObjectHydrator) {
         getService.get('http://riftrun.local/v1/posts')
                   .subscribe(response => this.items = response._embedded.items);
+
+        this.hydrate(this.items);
+    }
+
+    public hydrate(items:Array<string>) {
+        let hydrated = [];
+        items.forEach(function(item) {
+            console.log(item);
+        });
     }
 }
