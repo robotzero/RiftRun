e33dfc968fc851a37880f0a6fe3acf490ed25c63
Agnieszka K. Kocinski <agnieszka.kocinski@gmail.com>
2015-11-14 15:39:54 +0000

    Models and transforming json into Value objects.

--- fa/components/post/post.ts
+++ fa/components/post/post.ts
@@ -1,10 +1,14 @@
 import {Component, View, NgFor, NgIf, CORE_DIRECTIVES} from 'angular2/angular2';
 import {APIGetService} from '../../services/apigetservice';
-import {ObjectHydrator} from "../../services/objecthydrator";
+import {PostQuery} from "../../models/postquery";
+import {Player} from "../../models/player";
+import {Query} from "../../models/query";
+import {GameType} from "../../models/gametype";
+import {CharacterType} from "../../models/charactertype";
 
 @Component({
     selector: 'post',
-    providers:[APIGetService, ObjectHydrator]
+    providers:[APIGetService]
 })
 @View({
     templateUrl: './components/post/post.html',
@@ -13,18 +17,35 @@ import {ObjectHydrator} from "../../services/objecthydrator";
 
 export class Post {
     items: Array<string>;
+    posts: Array<PostQuery> = [];
 
-    constructor(getService:APIGetService, public hydrator: ObjectHydrator) {
+    constructor(getService:APIGetService) {
         getService.get('http://riftrun.local/v1/posts')
-                  .subscribe(response => this.items = response._embedded.items);
-
-        this.hydrate(this.items);
-    }
-
-    public hydrate(items:Array<string>) {
-        let hydrated = [];
-        items.forEach(function(item) {
-            console.log(item);
-        });
+                  .map((posts: any) => {
+                      let result:Array<PostQuery> = [];
+                      let characterTypes:Array<CharacterType> = [];
+                      posts._embedded.items.forEach((post) => {
+                          characterTypes.push(post.query.characterType.concat());
+                          result.push(new PostQuery(
+                              new Player(
+                                  post.player.id,
+                                  post.player.type,
+                                  post.player.paragonPoints,
+                                  post.player.battleTag,
+                                  post.player.region,
+                                  post.player.seasonal,
+                                  post.player.gametype
+                              ),
+                              new Query(
+                                  post.query.id,
+                                  post.query.minParagon,
+                                  new GameType(post.query.game.level, post.query.game.type),
+                                  characterTypes
+                              )
+                          ));
+                      });
+                      return result;
+                  })
+                  .subscribe(response => this.posts = response);
     }
 }
