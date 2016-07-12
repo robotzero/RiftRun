c5da1b521438e8a097c75a77111831d93df8a392
Agnieszka K. Kocinski <0robotzero@gmail.com>
2016-07-12 21:14:12 +0100

    Stabilizing the app. Disabled forms for now.

--- fa/app/app.ts
+++ fa/app/app.ts
@@ -3,19 +3,20 @@
 
 import {Component, bind} from '@angular/core';
 import {bootstrap} from '@angular/platform-browser-dynamic';
-
+import {ROUTER_DIRECTIVES} from '@angular/router';
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import {HTTP_PROVIDERS} from '@angular/http';
-import {MdToolbar} from '@angular2-material/toolbar';
+// import {MdToolbar} from '@angular2-material/toolbar';
 import {APP_ROUTER_PROVIDER} from "./routes";
-import { disableDeprecatedForms, provideForms } from '@angular/forms';
+//import {provideForms, disableDeprecatedForms} from "@angular/forms";
 
 // Annotation section
 @Component({
     selector: 'my-app',
     templateUrl: './app/app.html',
     styleUrls: ['./app/app.css'],
-    directives: [, MdToolbar]
+    directives: [ROUTER_DIRECTIVES]
+    // directives: [MdToolbar]
 })
 
 // Component controller
@@ -24,5 +25,6 @@ class MyAppComponent {
 }
 
 bootstrap(MyAppComponent, [
-    HTTP_PROVIDERS, APP_ROUTER_PROVIDER, disableDeprecatedForms(), provideForms(), bind(LocationStrategy).toClass(HashLocationStrategy)
+    //HTTP_PROVIDERS, APP_ROUTER_PROVIDER, disableDeprecatedForms(), provideForms(), bind(LocationStrategy).toClass(HashLocationStrategy)
+    HTTP_PROVIDERS, APP_ROUTER_PROVIDER, bind(LocationStrategy).toClass(HashLocationStrategy)
 ]);
--- fa/app/components/post/post.ts
+++ fa/app/components/post/post.ts
@@ -5,19 +5,17 @@ import {PostQuery} from '../../models/postquery';
 import {APIPostService} from '../../services/apipostservice';
 import {Component, OnInit} from '@angular/core';
 import {CORE_DIRECTIVES, NgFor, NgIf} from '@angular/common';
-import { MdCard } from '@angular2-material/card';
-import { MdInput } from '@angular2-material/input';
-import { MdButton } from '@angular2-material/button';
 import {PostFactory} from "../../utils/postFactory";
 import {ModelSelector} from "../../directives/modelselector";
-import {FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES, FormBuilder, FormGroup} from '@angular/forms';
+// import {FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES, FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
     selector: 'post',
     providers:[APIGetService, APIPostService, PostFactory],
-    viewProviders: [FormBuilder],
-    templateUrl: './app/components/post/post.html',
-    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES, ModelSelector, MdCard, MdInput, MdButton, NgFor, NgIf]
+    // viewProviders: [FormBuilder],
+    templateUrl: './app/components/post/post_new.html',
+    directives: [CORE_DIRECTIVES, ModelSelector, NgFor, NgIf]
+    //directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES, ModelSelector, MdCard, MdInput, MdButton, NgFor, NgIf]
 })
 
 export class Post implements OnInit {
@@ -26,8 +24,8 @@ export class Post implements OnInit {
     postService: APIPostService;
     getService: APIGetService;
     postFactory: PostFactory;
-    postForm: FormGroup;
-    formBuilder: FormBuilder;
+    // postForm: FormGroup;
+    // formBuilder: FormBuilder;
     response: any;
     playerTypes: Array<string> = ['Demon Hunter', 'Wizard', 'Witch Doctor', 'Barbarian', 'Crusader'];
     playerRegions: Array<string> = ['EU', 'US', 'ASIA'];
@@ -35,17 +33,17 @@ export class Post implements OnInit {
     queryGameTypes: Array<string> = ['Rift', 'Grift', 'PowerGrift', 'Bounties', 'Keywardens', 'Ubers'];
 
     ngOnInit():void {
-        this.postForm = this.formBuilder.group({
-            playerType: ['Demon Hunter'],
-            playerParagonPoints: [''],
-            playerBattleTag: [''],
-            playerRegion: ['EU'],
-            playerGameType: ['seasonal'],
-            queryMinParagon: ['15'],
-            queryGameLevel: ['40+'],
-            queryGameType: ['grift'], 
-            queryCharacterType: ['demon hunter']
-        });
+    //     this.postForm = this.formBuilder.group({
+    //         playerType: ['Demon Hunter'],
+    //         playerParagonPoints: [''],
+    //         playerBattleTag: [''],
+    //         playerRegion: ['EU'],
+    //         playerGameType: ['seasonal'],
+    //         queryMinParagon: ['15'],
+    //         queryGameLevel: ['40+'],
+    //         queryGameType: ['grift'],
+    //         queryCharacterType: ['demon hunter']
+    //     });
 
         this.getService.get('http://riftrun.local/v1/posts')
             .map((posts: any) => {
@@ -54,11 +52,12 @@ export class Post implements OnInit {
             .subscribe(response => this.posts = response);
     }
 
-    constructor(getService:APIGetService, postService:APIPostService, formBuilder: FormBuilder, postFactory: PostFactory) {
+    // constructor(getService:APIGetService, postService:APIPostService, formBuilder: FormBuilder, postFactory: PostFactory) {
+    constructor(getService:APIGetService, postService:APIPostService, postFactory: PostFactory) {
         this.getService = getService;
         this.postService = postService;
         this.postFactory = postFactory;
-        this.formBuilder = formBuilder;
+        //this.formBuilder = formBuilder;
     }
 
     postContent(item:any) {
