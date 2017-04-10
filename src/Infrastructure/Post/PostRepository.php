02a81730a44efbdaf73aead543bf7367b1d9a703
Agnieszka K. Kocinski <0robotzero@gmail.com>
2017-04-10 20:42:38 +0100

    Trying to wire up new repository.

--- src/Infrastructure/Post/PostRepository.php
+++ src/Infrastructure/Post/PostRepository.php
@@ -4,8 +4,25 @@ namespace App\Infrastructure\Post;
 
 use App\Domain\Post\Repository\PostRepositoryInterface;
 use Infrastructure\Common\Doctrine\ORM\EntityRepository;
+use Pagerfanta\Pagerfanta;
 
+/**
+ * Class PostRepository
+ * @package App\Infrastructure\Post
+ */
 class PostRepository extends EntityRepository implements PostRepositoryInterface
 {
+    public function findAll() : Pagerfanta {
+        $queryBuilder = $this->createQueryBuilder($alias = 'posts');
+        $queryBuilder->select('posts')
+            ->innerJoin('posts.query', 'q')
+            ->innerJoin('posts.player', 'p')
+            ->innerJoin('q.game', 'g')
+            ->innerJoin('q.characterType', 'ct')
+            ->orderBy('posts.createdAt', 'desc')
+            ->add('where', 'posts.createdAt > :createdAt')
+            ->setParameter('createdAt', new \DateTime('-1 month'), \Doctrine\DBAL\Types\Type::DATETIME);
 
+        return $this->getPaginator($queryBuilder);
+    }
 }
